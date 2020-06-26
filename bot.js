5c0e20ddf19cb2c4a7817bc02de92f09198d0dd5
Eddy Gray <dxd_eddy@protonmail.com>
2020-06-26 17:31:14 +0100

    Updated Deanbot - TESTING

--- bot.js
+++ bot.js
@@ -12,6 +12,10 @@ const {Client, Attachment} = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 
+const ytdl = require('ytdl-core')
+const prefix = "?"
+
+
 var date = new Date();
 
 const eddyUserID = "239399070454644736"; 
@@ -31,9 +35,9 @@ const deanbarryVideoFeedIDArray = ['380045599724142594','239399070454644736','36
 
 //Angela, Eddy, Jack, Liam, Lain
 const deanbarryPictureSubscriptionIDArray = ['239414210277277708','239399070454644736','402881756656566292', '380045599724142594','508070749748068367']
-//Lain, Me
-const tempDeanbarryPictureSubscriptionIDArray = ['239399070454644736']
-const noImportantValePictureSubscriptionIDArray = ['508070749748068367']
+
+//CEO ID List - Eddy, Jack, Herlander, Ciaran, Liam
+const ceoidlist = ['239399070454644736','402881756656566292','184362101903458306','201045084764897280','380045599724142594']
 
 //Array of profile pics for the bot
 const deanBarryPictureArray = ['https://i.imgur.com/ZGG1coY.jpg','https://i.imgur.com/GkKHZ6g.jpg','https://i.imgur.com/7eaxzyF.jpg','https://i.imgur.com/x9YI637.jpg','https://i.imgur.com/U3ndiif.jpg','https://i.imgur.com/lojVszj.jpg','https://i.imgur.com/YXT77gT.jpg','https://i.imgur.com/41YgnKX.jpg','https://i.imgur.com/etqbkJF.jpg','https://i.imgur.com/CzZYE8V.jpg','https://i.imgur.com/w7p2evs.jpg','https://i.imgur.com/yN0lGPm.jpg','https://i.imgur.com/itoJLCz.jpg','https://i.imgur.com/2HnqLR0.jpg','https://i.imgur.com/HvzOwaX.jpg','https://i.imgur.com/iQhr0hh.jpg','https://i.imgur.com/fOyleut.jpg','https://i.imgur.com/r8Opgti.jpg','https://i.imgur.com/s29yllp.png','https://i.imgur.com/nei8JNb.jpg','https://i.imgur.com/Z2LIEbx.jpg','https://i.imgur.com/XWWc1Fk.jpg','https://i.imgur.com/nGICHOP.jpg','https://i.imgur.com/9uPaJHW.jpg','https://i.imgur.com/Ec690T2.jpg','https://i.imgur.com/F3koQ6V.jpg','https://i.imgur.com/rWzWmev.jpg','https://i.imgur.com/kYW2VSR.jpg','https://i.imgur.com/1LWEy2Y.jpg','https://i.imgur.com/itm7huY.jpg','https://i.imgur.com/y91eeym.jpg','https://i.imgur.com/8s9ffkN.jpg','https://i.imgur.com/O5OWBlk.jpg','https://i.imgur.com/g3aK5oS.jpg','https://i.imgur.com/nABAcmj.jpg','https://i.imgur.com/OOBBruh.jpg','https://i.imgur.com/KGuITtN.jpg','https://i.imgur.com/klO5b3u.jpg','https://i.imgur.com/5iYVmxG.jpg','https://i.imgur.com/TvuDmCS.jpg','https://i.imgur.com/X6ay2pk.jpg','https://i.imgur.com/NfReQRR.jpg','https://i.imgur.com/uvKOLUW.png','https://i.imgur.com/NHs44Bi.png','https://i.imgur.com/kLkgrJZ.png','https://i.imgur.com/QIyXPnu.png','https://i.imgur.com/rhsTuiA.png','https://i.imgur.com/UzkInUS.png','https://i.imgur.com/UzkInUS.png','https://i.imgur.com/x3Z4RJb.png','https://i.imgur.com/etzJWwt.png','https://i.imgur.com/Kixu5xo.png','https://i.imgur.com/frG7xmQ.png','https://i.imgur.com/9EPabKn.png','https://i.imgur.com/XUZHw5B.png','https://i.imgur.com/X2BTB6E.png']
@@ -76,7 +80,7 @@ var i;
  *
 */
 
-function deanSetProfile () {
+function denanSetProfile () {
 
 	client.user.setAvatar(deanBarryPictureArray[generateRandomNumber(deanBarryPictureArray.length)])
 
@@ -113,6 +117,12 @@ function sendChannelAMessage (id, content) {
 	channel.send(content)
 }
 
+function CEOAlert (content) {
+	for (i = 0; 9 < ceoidlist.length; i++) {
+		sendUserAMessage(ceoidlist[i],content)
+	}
+}
+
 /*
  * 
  * The majority of the logic will go here
@@ -128,173 +138,257 @@ client.on('ready', () => {
 
 	console.log("Ready")
 	
-	sendUserAMessage(eddyUserID,"Logged is as: "+client.user.tag)
+	//sendUserAMessage(eddyUserID,"Logged is as: "+client.user.tag)
 
 	//sendOutDBPicture (deanBarryPictureArray,messagearray,tempDeanbarryPictureSubscriptionIDArray)
 	setInterval(sendOutDBPicture, 52400000, deanBarryPictureArray,messagearray,deanbarryPictureSubscriptionIDArray)
 
 	//setInterval(sendUserAMessage,10000,eddyUserID,"test")
 
-	deanSetProfile()
-	setInterval(deanSetProfile, 1000000)
+	//deanSetProfile()
+	//setInterval(deanSetProfile, 1000000)
 
 });
 
 
 
 client.on('message', msg => {
-//Start of message event code
-
-message = msg.content;
-lowerMessage = message.toLowerCase();
-
-
-if(msg.author.bot) return;
-
-//Bot DM forwarding code -- FINISHED
-if(msg.channel.type === "dm") {
-	sendUserAMessage(eddyUserID,"I received a message from: "+msg.author.toString()+" they sent:\n "+msg.content)
-};
-
-//Ping - FINISHED
-if (message == "ping") {
-	msg.reply("pong")
-	msg.react("👌");
-}
-
-//Deanbarry video feed code -- FINISHED
-if (message.substring(0,1) == "!" && msg.author.id == "239399070454644736") {
-	for (i = 0; i < deanbarryVideoFeedIDArray.length; i++) {
-		sendUserAMessage(deanbarryVideoFeedIDArray[i],"----------------------------------\nYou've received a message: \n"+message.substring(1))
+	//Start of message event code
+	
+	
+	
+	message = msg.content;
+	lowerMessage = message.toLowerCase();
+	
+	
+	if(msg.author.bot) return;
+	
+	//Bot DM forwarding code -- FINISHED
+	if(msg.channel.type === "dm") {
+		sendUserAMessage(eddyUserID,"I received a message from: "+msg.author.toString()+" they sent:\n "+msg.content)
+	};
+	
+	//Ping - FINISHED
+	if (message == "ping") {
+		msg.reply("pong")
+		msg.react("👌");
 	}
-}
-
-//Russian Roulette code -- FINISHED
-if(message == "russianroulette" || message == "russian roulette") {
-	var revolver = generateRandomNumber(9999998)
-	msg.reply("10,000,000 deletes #general - you got: "+revolver)
-	msg.react("👌");
-}
-
-//Cheers Dean code -- FINISHED
-if (cheersdean.includes(lowerMessage)) {
-	msg.reply(`
-		https://www.patreon.com/deanbarry/posts
-		https://www.youtube.com/user/deanbarry1971
-		https://twitter.com/deanBarry
-		`);
-	msg.react('👌');
-}
-
-//Wardropdrop picker -- REMOVED
-
-//Welcome - FINISHED
-if (message == "welcome") {
-	var embedImage = new Discord.MessageEmbed()
-		.setTitle('WELCOME TO DXD CLAN')
-		.attachFiles("https://i.imgur.com/0SnJEMv.png")
-		//.setImage("https://i.imgur.com/0SnJEMv.png")
-	msg.reply(embedImage)
-	msg.react("👌");
-}
-
-//I'm feeling creative -- FINISHED
-if (creativearray.includes(lowerMessage)) {
-	var embedImage = new Discord.MessageEmbed()
-		.setTitle("I\'m feeling creative ")
-		.attachFiles(artarray[generateRandomNumber(artarray.length)])
-		//.setImage(artarray[generateRandomNumber(artarray.length)])
-	msg.reply(embedImage)
-	msg.react("👌");
-}
-
-//Funny word detector -- FINISHED
-if (lowerMessage.includes("nigge")) {
-	msg.reply("HAHA FUNNY WORD");
-	msg.react('👌');
-}
-
-//Coomer detector -- FINISHED
-if (lowerMessage.includes("coom") || lowerMessage.includes(" coom ") || lowerMessage.includes(" coomer ")) {
-	var embedImage = new Discord.MessageEmbed()
-		.setTitle("COOMER SPOTTED")
-		.attachFiles('https://i.imgur.com/IHvRQvG.png')
-		//.setImage('https://i.imgur.com/IHvRQvG.png')
-	msg.reply(embedImage)
-	msg.react('👌');
-}
-
-//Boomer detector -- FINISHED
-if (lowerMessage.includes("boomer")) {
-	var embedImage = new Discord.MessageEmbed()
-		.setTitle("BOOMER SPOTTED")
-		.attachFiles('https://i.imgur.com/mY5RGIX.png')
-		//.setImage('https://i.imgur.com/mY5RGIX.png')
-		msg.reply(embedImage)
-	msg.react('👌');
-}
-
-//Vibe check -- FINISHED
-if (lowerMessage === "vibe check") {
-	if (msg.channel.type === "dm") return msg.reply("You cannot do that in DM");
-	if (!msg.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return msg.reply('Missing the required `Move Members` permission.');
-
-	var vibeRoll = generateRandomNumber(8)
-	var imageRoll = generateRandomNumber(vibearray.length)
-
-	var member = msg.member;
-
-
-	if (vibeRoll == 1) {
-		msg.reply("You have passed the vibe check")
+	
+	//Deanbarry video feed code -- FINISHED
+	if (message.substring(0,1) == "!" && msg.author.id == "239399070454644736") {
+		for (i = 0; i < deanbarryVideoFeedIDArray.length; i++) {
+			sendUserAMessage(deanbarryVideoFeedIDArray[i],"----------------------------------\nYou've received a message: \n"+message.substring(1))
+		}
+	}
+	
+	//Russian Roulette code -- FINISHED
+	if(message == "russianroulette" || message == "russian roulette") {
+		var revolver = generateRandomNumber(9999998)
+		msg.reply("10,000,000 deletes #general - you got: "+revolver)
+		msg.react("👌");
+	}
+	
+	//Cheers Dean code -- FINISHED
+	if (cheersdean.includes(lowerMessage)) {
+		msg.reply(`
+			https://www.patreon.com/deanbarry/posts
+			https://www.youtube.com/user/deanbarry1971
+			https://twitter.com/deanBarry
+			`);
 		msg.react('👌');
-		// ADDITION - happy image?
 	}
-	else if (vibeRoll > 1 && vibeRoll < 6) {
-		member.voice.kick()
+	
+	//Wardropdrop picker -- REMOVED
+	
+	//Welcome - FINISHED
+	if (message == "welcome") {
 		var embedImage = new Discord.MessageEmbed()
-			.setTitle("YOU HAVE FAILED THE VIBE CHECK - GIVE ME YOUR PENIS LICENSE")
-			.attachFiles(vibearray[imageRoll])
-			//.setImage(vibearray[imageRoll])
+			.setTitle('WELCOME TO DXD CLAN')
+			.attachFiles("https://i.imgur.com/0SnJEMv.png")
+			//.setImage("https://i.imgur.com/0SnJEMv.png")
 		msg.reply(embedImage)
-		msg.react('👌');
-
+		msg.react("👌");
 	}
-	else {
-		member.voice.kick()
+	
+	//I'm feeling creative -- FINISHED
+	if (creativearray.includes(lowerMessage)) {
 		var embedImage = new Discord.MessageEmbed()
-			.setTitle("I WILL NOW PERFORM COCK AND BALL TORTURE ON THE EPIC LOSER")
-			.attachFiles(vibearray[imageRoll])
-			//.setImage(vibearray[imageRoll])
+			.setTitle("I\'m feeling creative ")
+			.attachFiles(artarray[generateRandomNumber(artarray.length)])
+			//.setImage(artarray[generateRandomNumber(artarray.length)])
+		msg.reply(embedImage)
+		msg.react("👌");
+	}
+	
+	//Funny word detector -- FINISHED
+	if (lowerMessage.includes("nigge")) {
+		msg.reply("HAHA FUNNY WORD");
+		msg.react('👌');
+	}
+	
+	//Coomer detector -- FINISHED
+	if (lowerMessage.includes("coom") || lowerMessage.includes(" coom ") || lowerMessage.includes(" coomer ")) {
+		var embedImage = new Discord.MessageEmbed()
+			.setTitle("COOMER SPOTTED")
+			.attachFiles('https://i.imgur.com/IHvRQvG.png')
+			//.setImage('https://i.imgur.com/IHvRQvG.png')
 		msg.reply(embedImage)
 		msg.react('👌');
 	}
-}
+	
+	//Boomer detector -- FINISHED
+	if (lowerMessage.includes("boomer")) {
+		var embedImage = new Discord.MessageEmbed()
+			.setTitle("BOOMER SPOTTED")
+			.attachFiles('https://i.imgur.com/mY5RGIX.png')
+			//.setImage('https://i.imgur.com/mY5RGIX.png')
+			msg.reply(embedImage)
+		msg.react('👌');
+	}
+	
+	//Vibe check -- FINISHED
+	if (lowerMessage === "vibe check") {
+		if (msg.channel.type === "dm") return msg.reply("You cannot do that in DM");
+		if (!msg.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return msg.reply('Missing the required `Move Members` permission.');
+	
+		var vibeRoll = generateRandomNumber(8)
+		var imageRoll = generateRandomNumber(vibearray.length)
+	
+		var member = msg.member;
+	
+	
+		if (vibeRoll == 1) {
+			msg.reply("You have passed the vibe check")
+			msg.react('👌');
+			// ADDITION - happy image?
+		}
+		else if (vibeRoll > 1 && vibeRoll < 6) {
+			member.voice.kick()
+			var embedImage = new Discord.MessageEmbed()
+				.setTitle("YOU HAVE FAILED THE VIBE CHECK - GIVE ME YOUR PENIS LICENSE")
+				.attachFiles(vibearray[imageRoll])
+				//.setImage(vibearray[imageRoll])
+			msg.reply(embedImage)
+			msg.react('👌');
+	
+		}
+		else {
+			member.voice.kick()
+			var embedImage = new Discord.MessageEmbed()
+				.setTitle("I WILL NOW PERFORM COCK AND BALL TORTURE ON THE EPIC LOSER")
+				.attachFiles(vibearray[imageRoll])
+				//.setImage(vibearray[imageRoll])
+			msg.reply(embedImage)
+			msg.react('👌');
+		}
+	}
+	
+	//This is so sad -- FINISHED
+	if (sadArrayPhrases.includes(lowerMessage)) {
+		var sadArrayIndex = generateRandomNumber(sadImageArray.length)
+		var embedImage = new Discord.MessageEmbed()
+			.setTitle("This is so sad...")
+			.attachFiles(sadImageArray[sadArrayIndex])
+			//.setImage(sadImageArray[sadArrayIndex])// - This duplicates Kaneki image for some reason?
+		msg.reply(embedImage)
+		msg.react('👌');
+	}
+	
+	//Nonce detector -- FINISHED
+	if (lowerMessage.includes("nonce")) {
+		var nonceIndex = generateRandomNumber(noncereactionpictures.length)
+		var embedImage = new Discord.MessageEmbed()
+			.setTitle("Please take a seat right over there sir...")
+			.attachFiles(noncereactionpictures[nonceIndex])
+			//.setImage(noncereactionpictures[nonceIndex])
+		msg.reply(embedImage)
+		msg.react('👌');
+	}
+	
+	/*
+	*
+	*
+	*
+	*/
+	if (lowerMessage.includes("nose")) {
+		/*
+		var voiceChannel = msg.member.voice.channel;
+		voiceChannel.join().then(connection => {
+			//const stream = ytdl('https://www.youtube.com/watch?v=jcYzZjIpSek', { filter: 'audioonly' });
+			const dispatcher = connection.play('audiofile.mp3');
+	
+			dispatcher.on('end', () => voiceChannel.leave());
+		});
+		*/
+	
+		/*
+		if (msg.channel.type === "dm") return msg.reply("You cannot do that in DM");
+		var voiceChannel = msg.member.voice.channel;
+		//const dispatcher = connection.playFile('./audiofile.mp3');
+		var connection = voiceChannel.join()
+		//var dispatcher = connection.
+		voiceChannel.leave();
+		*/
+		/*
+		var voiceChannel = msg.member.voice.channel;
+		var connection = voiceChannel.join()
+		const broadcast = client.voice.createBroadcast();
+		const dispatcher = broadcast.play('audiofile.mp3');
+		connection1.play(broadcast);
+		*/
+		
+		//
+	}
+	
+	
+	
+	
+	/*
+	*
+	*
+	*
+	*/
+	
+	//End of message event code
+	});
+/*
+client.on("message", async message => {
+	if(message.author.bot) return
+	if(!message.content.startsWith(prefix)) return
 
-//This is so sad -- FINISHED
-if (sadArrayPhrases.includes(lowerMessage)) {
-	var sadArrayIndex = generateRandomNumber(sadImageArray.length)
-	var embedImage = new Discord.MessageEmbed()
-		.setTitle("This is so sad...")
-		.attachFiles(sadImageArray[sadArrayIndex])
-		//.setImage(sadImageArray[sadArrayIndex])// - This duplicates Kaneki image for some reason?
-	msg.reply(embedImage)
-	msg.react('👌');
-}
+	const args = message.content.substring(prefix.length).split(" ")
 
-//Nonce detector -- FINISHED
-if (lowerMessage.includes("nonce")) {
-	var nonceIndex = generateRandomNumber(noncereactionpictures.length)
-	var embedImage = new Discord.MessageEmbed()
-		.setTitle("Please take a seat right over there sir...")
-		.attachFiles(noncereactionpictures[nonceIndex])
-		//.setImage(noncereactionpictures[nonceIndex])
-	msg.reply(embedImage)
-	msg.react('👌');
-}
+	if (message.content.startsWith("play")) {
+		const voiceChannel = message.member.voice.channel
+		if (!voiceChannel) return message.channel.send("Please enter a voice channel to play music")
+		const permissions = voiceChannel.permissionsFor(message.client.user)
+		if(!permissions.has("CONNECT")) return message.channel.send("I don't have permission to connect to the voice channel")
+		if(!permissions.has("SPEAK")) return message.channel.send("I don't have permission to speak in that channel")
+		
+		try {
+			var connection = await voiceChannel.join()
+		} catch (error) {
+			console.log("There was an error connecting to the voice channel: ${error}")
+			return message.channel.send("There was an error connecting to the voice channel: ${error}")
+		}
 
-//End of message event code
-});
+		//const dispatcher = connection.play(ytdl(args[1]))
+		const dispatcher = connection.play("./audiofile.mp3")
+		.on("finish", () => {
+			voiceChannel.leave()
+		})
+		.on('error', error => {
+			console.log(error)
+		})
+		dispatcher.setVolumeLogarithmic(5 / 5)
+	} else if (message.content.startsWith("stop")) {
+		if(!message.member.channel) return message.channel.send ("Please be in a channel to stop the music")
+		message.member.voice.channel.leave()
+		return undefined
+	}
+})
+
+*/
 
 //DEBUG INFO
 
@@ -303,7 +397,8 @@ client.on("channelCreate", function(channel){
 	if (channel.name == "undefined" || channel.name == undefined) {
 		return;
 	} else{
-	sendUserAMessage("239399070454644736","	- A " + channel.type + " channel called: \"" + channel.name + "\" was created. The time was: "+ currenttime + "\n-----------------------\n")
+	//sendUserAMessage("239399070454644736","	- A " + channel.type + " channel called: \"" + channel.name + "\" was created. The time was: "+ currenttime + "\n-----------------------\n")
+	CEOAlert("	- A " + channel.type + " channel called: \"" + channel.name + "\" was created. The time was: "+ currenttime + "\n-----------------------\n")
 }});
 
 client.on("channelDelete", function(channel){
@@ -311,13 +406,21 @@ client.on("channelDelete", function(channel){
 	if (channel.name == "undefined") {
 		return;
 	} else{
-	sendUserAMessage("239399070454644736","	- A " + channel.type + " channel called: \"" + channel.name + "\" was deleted. The time was: "+ currenttime + "\n-----------------------\n")
+	//sendUserAMessage("239399070454644736","	- A " + channel.type + " channel called: \"" + channel.name + "\" was deleted. The time was: "+ currenttime + "\n-----------------------\n")
+	CEOAlert("	- A " + channel.type + " channel called: \"" + channel.name + "\" was deleted. The time was: "+ currenttime + "\n-----------------------\n")
 }});
 
 client.on("guildMemberRemove", function(member){
 	var currenttime = date.toTimeString();
-	sendUserAMessage("239399070454644736","Someone left or was kicked. " + member.user.username + " - the time was: " + currenttime)
+	//sendUserAMessage("239399070454644736","Someone left or was kicked. " + member.user.username + " - the time was: " + currenttime)
 	sendChannelAMessage("494651540561330217","Someone left or was kicked, their username was: " + member.user.username + " - the time was: " + currenttime)
+	CEOAlert("Someone left or was kicked, their username was: " + member.user.username + " - the time was: " + currenttime)
+});
+
+client.on("messageDelete",  function(message) {
+	var currenttime = date.toTimeString();
+	sendUserAMessage("239399070454644736","A message was deleted... It's content was: \n\"" + message.content + "\"\nIt was sent at: " + message.createdAt + ". It was sent by User: " + message.author.username + " In channel: " + message.channel.name)
+	CEOAlert("A message was deleted... It's content was: \n\"" + message.content + "\"\nIt was sent at: " + message.createdAt + ". It was sent by User: " + message.author.username + " In channel: " + message.channel.name)
 });
 
 /*
@@ -341,10 +444,7 @@ client.on("guildMemberUpdate", function(oldMember, newMember){
 }});
 */
 
-client.on("messageDelete",  function(message) {
-	var currenttime = date.toTimeString();
-	sendUserAMessage("239399070454644736","A message was deleted... It's content was: \n\"" + message.content + "\"\nIt was sent at: " + message.createdAt + ". It was sent by User: " + message.author.username + " In channel: " + message.channel.name)
-});
+
 
 
 client.login(config.token);
